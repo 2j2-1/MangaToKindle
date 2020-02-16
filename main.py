3a860117607ee04be8c7e8e2e710d2b2686ee281
2j2-1 <jbrightmore@protonmail.com>
2020-02-16 20:45:10 +0000

    Added Convert

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import urllib.request as urllib
 import requests
 import os
+import shutil
+import subprocess
 
 opener = urllib.build_opener()
 opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
@@ -11,13 +13,12 @@ manga = "dr-stone"
 
 class Manga(object):
     """docstring for Manga"""
-    url = ''
 
-    def __init__(self, manga):
-        self.manga = manga
-        self.mangaURL = "{}/{}".format(self.url, self.manga)
+    def __init__(self, mangaName):
+        self.mangaName = mangaName
+        self.mangaURL = "{}/{}".format(self.url, self.mangaName)
         self.soup = self.getSoup(self.mangaURL)
-        self.filePath = manga
+        self.filePath = mangaName
         self.lengthOfManga = self.getLengthOfManga()
 
     def getSoup(self, url):
@@ -32,11 +33,14 @@ class Manga(object):
 
     def getChapter(self, chapter):
         lengthOfChapter = self.getLengthOfChapter(chapter)
-        filePath = "{}/{}/".format(self.manga, chapter)
+        filePath = "{}/{}/".format(self.mangaName, chapter)
         for image in range(1, lengthOfChapter + 1):
             imageLocation = self.getImageLocation(chapter, image)
             print(filePath, image)
-            # self.getImage(imageLocation, filePath, image)
+            self.getImage(imageLocation, filePath, image)
+        if self.convert:
+            self.convertToMobi(chapter)
+
 
     def getManga(self):
         self.getChapters(1, self.lengthOfManga)
@@ -48,10 +52,18 @@ class Manga(object):
             except:
                 print("Chapter {} is unavalible".format(chapter))
 
+    def convertToMobi(self,chapter):
+        subprocess.run(["kcc-c2e", "--profile=K34", "--format=MOBI", "--batchsplit=2", "--output=toSend/{}-{}.epub".format(
+            self.mangaName, chapter), "--title={}_{}".format(self.mangaName, chapter), self.filePath + "/" + str(chapter)])
+        if self.delete:  
+            shutil.rmtree('{}/{}'.format(self.filePath,chapter))
+
 
 class MangaPanda(Manga):
     """docstring for MangaPanda"""
     url = 'https://www.mangapanda.com'
+    convert = True
+    delete = True
 
     def getLengthOfManga(self):
         return int(self.soup.find(id="latestchapters").ul.li.a.get('href').split("/")[-1])
@@ -65,4 +77,4 @@ class MangaPanda(Manga):
         return soup.find(id='imgholder').img['src']
 
 manga = MangaPanda(manga)
-manga.getManga()
+manga.getChapters(start=2)
