5ed28305b9278973b7cc881858b69d5e0a2cc8fa
2j2-1 <jbrightmore@protonmail.com>
2020-02-16 18:38:22 +0000

    Put in class

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 from bs4 import BeautifulSoup
 import urllib.request as urllib
 import requests
+import os
 
 opener = urllib.build_opener()
 opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
 url = 'https://www.mangapanda.com'
 manga = "shingeki-no-kyojin"
-response = opener.open("{}/{}".format(url,manga)).read()
-soup = BeautifulSoup(response, 'html.parser')
 
+class Manga(object):
+	"""docstring for Manga"""
+	def __init__(self, url ,manga):
+		self.url = url
+		self.manga = manga
+		self.mangaURL = "{}/{}".format(self.url,self.manga)
+		self.soup = self.getSoup(self.mangaURL)
+		self.filePath = manga
 
-def getSoup(url):
-	response = opener.open(url).read()
-	return BeautifulSoup(response, 'html.parser')
+	def getSoup(self,url):
+		response = opener.open(url).read()
+		return BeautifulSoup(response, 'html.parser')
 
-def getLength():
-	return soup.find(id="latestchapters").ul.li.a.get('href').split("/")[-1]
+	def getLengthOfManga(self):
+		return self.soup.find(id="latestchapters").ul.li.a.get('href').split("/")[-1]
 
-def getLengthChapter(chapter):
-	soup = getSoup("{}/{}/{}".format(url,manga,chapter))
-	return int(soup.find(id="selectpage").find_all('option')[-1].string)
+	def getLengthOfChapter(self,chapter):
+		soup = self.getSoup("{}/{}".format(self.mangaURL,chapter))
+		return int(soup.find(id="selectpage").find_all('option')[-1].string)
 
-def getImageLocation(soup):
-	return soup.find(id='imgholder').img['src']
+	def getImageLocation(self,soup):
+		return soup.find(id='imgholder').img['src']
 
-def getImage(src,filePath,fileName):
-	with open('{}/{}.jpg'.format(filePath,fileName),'wb') as file:
-		file.write(requests.get(src).content)
+	def getImage(self,src,filePath,fileName):
+		if not os.path.exists(filePath):
+		    os.makedirs(filePath)
+		with open('{}/{}.jpg'.format(filePath,fileName),'wb') as file:
+			file.write(requests.get(src).content)
 
-def getChapter(chapter):
-	lengthOfChapter = getLengthChapter(chapter)
-	filePath = "{}/{}/".format(manga,chapter)
-	for image in range(1,lengthOfChapter+1):
-		soup = getSoup("{}/{}/{}/{}".format(url,manga,chapter,image))
-		imageLocation = getImageLocation(soup)
-		getImage(imageLocation,filePath,image)
-
-getChapter(1)
+	def getChapter(self,chapter):
+		lengthOfChapter = self.getLengthOfChapter(chapter)
+		filePath = "{}/{}/".format(self.manga,chapter)
+		for image in range(1,lengthOfChapter+1):
+			soup = self.getSoup("{}/{}/{}".format(self.mangaURL,chapter,image))
+			imageLocation = self.getImageLocation(soup)
+			self.getImage(imageLocation,filePath,image)
